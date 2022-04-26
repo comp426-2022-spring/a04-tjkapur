58e548b744933cad5d558bb4bf12c4ad22b0b101
“Terrence <tjkapur@live.unc.edu>
2022-04-26 16:08:55 -0400

    csd

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const database = require('better-sqlite3');
 const logdb = new database('log.db');
 
 const stmt = logdb.prepare(`
-SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
+SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
 );
 
 let row = stmt.get();
@@ -14,7 +14,7 @@ if (row == undefined) {
 
     console.log('Database is empty');
 
-    const sqlInit = `CREATE TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status INTEGER, referer TEXT, useragent TEXT);`;
+    const sqlInit = `CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT);`;
 
     logdb.exec(sqlInit);
 
